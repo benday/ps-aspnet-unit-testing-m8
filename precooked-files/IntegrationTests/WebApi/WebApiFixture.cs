e8dc78e232f7607569e9d0055063c43442c8764f
Benjamin Day <benday@benday.com>
2018-12-20 14:11:31 -0500

    Added some precooked files.

---
+++ precooked-files/IntegrationTests/WebApi/WebApiFixture.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Benday.Presidents.MvcIntegrationTests.AngularUi
+{
+    [TestClass]
+    public class WebApiFixture
+    {
+        private WebApplicationFactory<Benday.Presidents.AngularUi.Startup> _SystemUnderTest;
+        public WebApplicationFactory<Benday.Presidents.AngularUi.Startup> SystemUnderTest
+        {
+            get
+            {
+                if (_SystemUnderTest == null)
+                {
+                    _SystemUnderTest =
+                        new WebApplicationFactory<Benday.Presidents.AngularUi.Startup>();
+                }
+
+                return _SystemUnderTest;
+            }
+        }
+
+        [TestMethod]
+        public void GetStuff()
+        {
+            // var temp = GetInstanceOf<IOptions<MvcJsonOptions>>();
+
+            var temp = GetInstanceOf<IOptions<MvcOptions>>();
+
+            Console.WriteLine();          
+
+        }
+
+        [TestMethod]
+        public async Task GetAllPresidents_DefaultMediaTypeReturnedIsJson()
+        {
+            var client =
+                SystemUnderTest.CreateDefaultClient();
+
+            // NOTE: don't specify any special Accept header values
+            //       in order to verify default behavior
+
+            var response =
+                await client.GetAsync(
+                    "/api/president");
+
+            Assert.IsTrue(response.IsSuccessStatusCode,
+                "Should be a success status code but got '{0}'.",
+                response.StatusCode);
+
+            Assert.IsNotNull(response, "Response should not be null.");
+
+            Assert.AreNotEqual<string>(
+                String.Empty, response.Content.ToString(),
+                "response.Content should not be empty.");
+
+            Assert.AreEqual<string>("application/json",
+                response.Content.Headers.ContentType.MediaType,
+                "Media type header was wrong.");
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(responseBody);
+
+            Assert.AreNotEqual<string>(String.Empty, 
+                responseBody, "Response body should not be empty.");
+        }
+
+        [TestMethod]
+        public async Task GetAllPresidents_Xml()
+        {
+            var client =
+                SystemUnderTest.CreateDefaultClient();
+
+            var expectedContentType = "application/xml";
+
+            client.DefaultRequestHeaders
+                  .Accept
+                  .Add(new MediaTypeWithQualityHeaderValue(
+                      expectedContentType));
+
+            var response =
+                await client.GetAsync(
+                    "/api/president");
+
+            Assert.IsTrue(response.IsSuccessStatusCode,
+                "Should be a success status code but got '{0}'.",
+                response.StatusCode);
+
+            Assert.IsNotNull(response, "Response should not be null.");
+
+            Assert.AreNotEqual<string>(
+                String.Empty, response.Content.ToString(),
+                "response.Content should not be empty.");
+
+            Assert.AreEqual<string>(expectedContentType,
+                response.Content.Headers.ContentType.MediaType,
+                "Media type header was wrong.");
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(responseBody);
+
+            Assert.AreNotEqual<string>(String.Empty,
+                responseBody, "Response body should not be empty.");
+        }
+
+        [TestMethod]
+        public async Task GetAllPresidents_WhenRequestedContentTypeIsJsonReturnsResultsAsJson()
+        {
+            var client =
+                SystemUnderTest.CreateDefaultClient();
+
+            var expectedContentType = "application/json";
+
+            client.DefaultRequestHeaders
+                  .Accept
+                  .Add(new MediaTypeWithQualityHeaderValue(
+                      expectedContentType));
+
+            var response =
+                await client.GetAsync(
+                    "/api/president");
+
+            Assert.IsTrue(response.IsSuccessStatusCode,
+                "Should be a success status code but got '{0}'.",
+                response.StatusCode);
+
+            Assert.IsNotNull(response, "Response should not be null.");
+
+            Assert.AreNotEqual<string>(
+                String.Empty, response.Content.ToString(),
+                "response.Content should not be empty.");
+
+            Assert.AreEqual<string>(expectedContentType,
+                response.Content.Headers.ContentType.MediaType,
+                "Media type header was wrong.");
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(responseBody);
+
+            Assert.AreNotEqual<string>(String.Empty,
+                responseBody, "Response body should not be empty.");
+        }
+
+        private T GetInstanceOf<T>()
+        {
+            var client = SystemUnderTest.CreateDefaultClient();
+
+            var hostServices = SystemUnderTest.Server.Host.Services;
+
+            var scopeFactory = hostServices.GetService(
+                typeof(IServiceScopeFactory)) as IServiceScopeFactory;
+
+            using (IServiceScope scope = scopeFactory.CreateScope())
+            {
+                var returnValue =
+                    scope.ServiceProvider.GetService<T>();
+
+                Assert.IsNotNull(returnValue, "Could not get an instance of {0}.", typeof(T).Name);
+
+                return returnValue;
+            }
+        }
+
+
+    }
+}
---
+++ precooked-files/UnitTests/WebApiSerialization/PresidentJsonSerializationFixture.cs
@@ -0,0 +1,95 @@
+﻿using Benday.Presidents.Api.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Benday.Presidents.UnitTests.WebApiSerialization
+{
+    [TestClass]
+    public class PresidentJsonSerializationFixture
+    {
+        private JsonSerializer _SystemUnderTest;
+        public JsonSerializer SystemUnderTest
+        {
+            get
+            {
+                if (_SystemUnderTest == null)
+                {
+                    var contractResolver = new DefaultContractResolver
+                    {
+                        NamingStrategy = new CamelCaseNamingStrategy
+                        {
+                            OverrideSpecifiedNames = false
+                        }
+                    };
+
+                    _SystemUnderTest = new JsonSerializer();
+                    _SystemUnderTest.ContractResolver = contractResolver;
+                }
+
+                return _SystemUnderTest;
+            }
+        }
+
+        private string SerializeToJsonString(President value)
+        {
+            var builder = new StringBuilder();
+
+            SystemUnderTest.Serialize(
+                new StringWriter(builder), value);
+
+            return builder.ToString();
+        }
+
+        [TestMethod]
+        public void TermIsDeletedIsNotSerializedIntoJson()
+        {
+            var fromValue =
+                UnitTestUtility.GetGroverClevelandAsPresident();
+
+            var json = SerializeToJsonString(fromValue);
+
+            var presidentAsJson = JObject.Parse(json);
+
+            var terms = presidentAsJson["Terms"] as JArray;
+
+            Assert.IsNotNull(terms, "Terms was null or not an array.");
+            Assert.AreEqual<int>(2, terms.Count, "Unexpected term count");
+
+            var term = terms[0];
+
+            var isDeleted = term["IsDeleted"];
+
+            Assert.IsNull(isDeleted, "IsDeleted value should not exist.");
+        }
+
+        [TestMethod]
+        public void TermIsDeletedIsNotSerializedIntoJson_v2()
+        {
+            var fromValue =
+                UnitTestUtility.GetGroverClevelandAsPresident();
+
+            var json = SerializeToJsonString(fromValue);
+
+            var presidentAsJson = JObject.Parse(json);
+
+            var terms = presidentAsJson["terms"] as JArray;
+
+            Assert.IsNotNull(terms, "Terms was null or not an array.");
+            Assert.AreEqual<int>(2, terms.Count, "Unexpected term count");
+
+            var term = terms[0];
+
+            var isDeleted = term["isDeleted"];
+
+            Assert.IsNull(isDeleted, "IsDeleted value should not exist.");
+        }
+
+
+    }
+}
