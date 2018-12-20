a391450fb8606afb1d4ca583a49992534e44888b
Benjamin Day <benday@benday.com>
2018-12-20 14:07:57 -0500

    Added web api president controller.

---
+++ Benday.Presidents/src/Benday.Presidents.WebUi/Areas/Api/PresidentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Benday.Presidents.Api.Models;
+using Benday.Presidents.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Benday.Presidents.WebUi.Areas.Api
+{
+    [Route("api/[controller]")]
+    public class PresidentController : Controller
+    {
+        private IPresidentService _Service;
+
+        public PresidentController(IPresidentService service)
+        {
+            _Service = service;
+        }
+
+        // GET: api/President
+        [HttpGet]
+        public IEnumerable<President> Get()
+        {
+            return _Service.GetPresidents();
+        }
+
+        // GET: api/President/5
+        [HttpGet("{id}", Name = "Get")]
+        public President Get(int id)
+        {
+            return _Service.GetPresidentById(id);
+        }
+    }
+}
