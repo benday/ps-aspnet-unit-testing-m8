0d72f9fc4148eb2244cbba73c8d4c3a9557ba185
Benjamin Day <benday@benday.com>
2018-12-20 15:28:12 -0500

    Implemented integration test demo.

--- Benday.Presidents/src/Benday.Presidents.WebUi/Startup.cs
+++ Benday.Presidents/src/Benday.Presidents.WebUi/Startup.cs
@@ -66,7 +66,9 @@ namespace Benday.Presidents.WebUi
 
             RegisterTypes(services);
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddXmlSerializerFormatters();
             
             /*
             services.AddMvc()
--- Benday.Presidents/test/Benday.Presidents.MvcIntegrationTests/PresidentEncodingFixture.cs
+++ Benday.Presidents/test/Benday.Presidents.MvcIntegrationTests/PresidentEncodingFixture.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -28,28 +31,113 @@ namespace Benday.Presidents.MvcIntegrationTests
             }
         }
 
-        /*
+
         [TestMethod]
         public async Task GetAllPresidents_DefaultMediaTypeReturnedIsJson()
         {
-        }
-        */
+            // arrange
+            var client = SystemUnderTest.CreateDefaultClient();
+            var expectedContentType = "application/json";
 
-        /*
+            // act
+            var response =
+                await client.GetAsync(
+                    "/api/president"
+                );
+
+            // assert
+            Assert.IsTrue(response.IsSuccessStatusCode,
+                "Should be a success status code but got '{0}'.",
+                response.StatusCode
+            );
+
+            Assert.AreEqual<string>(
+                expectedContentType,
+                response.Content.Headers.ContentType.MediaType,
+                "Media type header was wrong."
+            );
+
+            var responseBody =
+                await response.Content.ReadAsStringAsync();
+
+            Assert.AreNotEqual<string>(String.Empty, responseBody,
+                "Response body should not be empty.");
+        }
+
         [TestMethod]
         public async Task GetAllPresidents_Xml()
         {
+            // arrange
+            var client = SystemUnderTest.CreateDefaultClient();
+            var expectedContentType = "application/xml";
 
+            client.DefaultRequestHeaders.Accept
+                .Add(new MediaTypeWithQualityHeaderValue(
+                    expectedContentType
+                ));
+
+            // act
+            var response =
+                await client.GetAsync(
+                    "/api/president"
+                );
+
+            // assert
+            Assert.IsTrue(response.IsSuccessStatusCode,
+                "Should be a success status code but got '{0}'.",
+                response.StatusCode
+            );
+
+            Assert.AreEqual<string>(
+                expectedContentType,
+                response.Content.Headers.ContentType.MediaType,
+                "Media type header was wrong."
+            );
+
+            var responseBody =
+                await response.Content.ReadAsStringAsync();
+
+            Assert.AreNotEqual<string>(String.Empty, responseBody,
+                "Response body should not be empty.");
         }
-        */
 
-        /*
         [TestMethod]
         public async Task GetAllPresidents_WhenRequestedContentTypeIsJsonReturnsResultsAsJson()
         {
-        }
-        */
+            // arrange
+            var client = SystemUnderTest.CreateDefaultClient();
+            var expectedContentType = "application/json";
 
+            client.DefaultRequestHeaders.Accept
+                .Add(new MediaTypeWithQualityHeaderValue(
+                    expectedContentType
+                ));
+
+            // act
+            var response =
+                await client.GetAsync(
+                    "/api/president"
+                );
+
+            // assert
+            Assert.IsTrue(response.IsSuccessStatusCode,
+                "Should be a success status code but got '{0}'.",
+                response.StatusCode
+            );
+
+            Assert.AreEqual<string>(
+                expectedContentType,
+                response.Content.Headers.ContentType.MediaType,
+                "Media type header was wrong."
+            );
+
+            var responseBody =
+                await response.Content.ReadAsStringAsync();
+
+            Assert.AreNotEqual<string>(String.Empty, responseBody,
+                "Response body should not be empty.");
+        }
+        
         private T GetInstanceOf<T>()
         {
             var client = SystemUnderTest.CreateDefaultClient();
